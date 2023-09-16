c3683fc9f89163e10fff55f2ca02b8c25e135665
AhmedAGadir <ahmed.gadir@gmail.com>
2023-09-16 07:21:11 +0100

    testing netlify forms

---
+++ (DEPRECATED)netlify/functions/subscribe.js
@@ -0,0 +1,59 @@
+// using netlify forms instead of serverless functions + mongodb
+// keeping the file incase i want to use it later
+
+const serverless = require("serverless-http");
+
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const cors = require("cors");
+const corsOptions = {
+	origin:
+		/https?:\/\/(www\.)?([a-z0-9]+--arabybuddylanding\.netlify\.app|arabybuddylanding\.netlify\.app|arabybuddy\.com)/,
+};
+
+app.use(cors(corsOptions));
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+	const { email } = req.body;
+
+	if (!email) {
+		return res.status(400).json({
+			message: "Email is required",
+		});
+	}
+
+	try {
+		const mongoose = require("mongoose");
+
+		await mongoose.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		const Email = require("./models/email.model");
+		const newEmail = new Email({ email });
+
+		await newEmail.save();
+
+		res.status(200).json({
+			message: "Thanks for subscribing! 🎉",
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: error.message || "Internal Server Error",
+		});
+	}
+});
+
+app.use("/.netlify/functions/subscribe", router);
+
+module.exports.handler = serverless(app);
